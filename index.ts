c38ed1c9ef1e644eec76afe70a7d4c98d55f5256
ayesha-adil <160041752+ayesha-adil@users.noreply.github.com>
2024-03-12 22:36:40 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,46 @@
+// let message :string ="ayesha!"
+// console.log(message);
+
+// let message="123"
+// console.log(message);
+
+
+// let personName :string ="ayesha"
+// // good day ayesha
+// console.log("Good day," + personName + "!")
+
+// console.log(`Good day ${personName}`);
+// primitive data types
+// string 
+// number 
+// boolen ture/falsi
+// null
+// undifined
+// let personName:string ="ayesha"
+// console.log(personName);
+// symbol
+// bigint
+// number
+// let personage:number = 50
+// let finalage:number = +7
+// console.log("finalAge," + Personage);
+// boolean type
+// let boolean :boolean=ture/fals
+// nulltype
+// let null :null = null
+
+let boolean:boolean=true;
+console.log(boolean);
+
+
+// data structure
+// object
+// Array
+let b:null;
+console.log(null);
+
+
+
+
+
+
